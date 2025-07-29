e10f6dbd2552eb11b565e5b60a64e511c2c8b84c
李棹洋 <15775988727@163.com>
2025-07-29 14:17:55 +0800

    Update init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,4 +1,9 @@
-require("core.options") -- 不需要加上lua文件夹是因为neovim会自己识别到这里,然后文件夹和其内的文件可以使用逗号隔开
-require("core.keymaps")
+vim.g.mapleader=" "
+vim.g.maplocalleader = "\\"
+vim.env.XDG_DATA_HOME = '/home/admin/.config/nvim'
+vim.env.XDG_CONFIG_HOME = '/home/admin/.config'
+package.path = '/home/admin/.config/nvim/lua/?.lua'
+require("config.options") -- 不需要加上lua文件夹是因为neovim会自己识别到这里,然后文件夹和其内的文件可以使用逗号隔开
+require("config.keymaps")
 require("config.lazy")
 require("config.autocmds")
